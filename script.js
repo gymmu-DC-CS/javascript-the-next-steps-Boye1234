60caff183ef2eb771e2d838f0145991e2bcbc2cf
boyeloff <boye.loeffel@gmail.com>
2023-12-14 15:34:08 +0100

    Did ex. 5&6 and FINALLY fixed ex 4

--- script.js
+++ script.js
@@ -48,12 +48,43 @@ export function exercise04(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
+    const previousElement = input[i - 1]
 
-    if (currentElement === " ") {
+    if (currentElement === " " && previousElement != "-") {
       count = count + 1
     } else if (currentElement === "0") {
       count = count - 1
     }
   }
   return count
+}
+
+export function exercise05(args) {
+  const input = args
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i].charCodeAt()
+
+    if (currentElement >= 65 && currentElement <= 90) {
+      return true
+    }
+  }
+  return false
+}
+
+export function exercise06(args) {
+  const input = args
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (
+      (currentElement >= 33 && currentElement <= 47) ||
+      (currentElement >= 58 && currentElement <= 64) ||
+      (currentElement >= 91 && currentElement <= 96) ||
+      (currentElement >= 123 && currentElement <= 126)
+    ) {
+      return true
+    }
+  }
+  return false
 }
